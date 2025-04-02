54e578c5eefbda8b499b3ecaf492f77c86515092
Shlok0606 <shlokpoona@gmail.com>
2025-04-02 12:59:54 +0530

    feat: added dropdown and Saving text on header

--- components/Dashboard/site-header.tsx
+++ components/Dashboard/site-header.tsx
@@ -1,6 +1,8 @@
-import { Button } from "@/components/ui/button"
-import { Separator } from "@/components/ui/separator"
-import { SidebarTrigger } from "@/components/ui/sidebar"
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { RepoDropdown } from "./repo-dropdown";
+
+const versions = ["repo-1", "repo-2", "repo-3"];
 
 export function SiteHeader() {
   return (
@@ -11,20 +13,11 @@ export function SiteHeader() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
-        <h1 className="text-base font-medium">Documents</h1>
-        <div className="ml-auto flex items-center gap-2">
-          <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
-            <a
-              href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
-              rel="noopener noreferrer"
-              target="_blank"
-              className="dark:text-foreground"
-            >
-              GitHub
-            </a>
-          </Button>
-        </div>
+        <h1 className="text-base font-medium">
+          <RepoDropdown versions={versions} defaultVersion={versions[0]} />
+        </h1>
+        <div className="ml-auto flex items-center gap-2">Saving...</div>
       </div>
     </header>
-  )
+  );
 }
