fe12441d8645c22f2f1357eba4d33b94d85bd6cd
Shlok0606 <shlokpoona@gmail.com>
2025-04-02 13:09:37 +0530

    feat: made show Saved text state based

--- components/Dashboard/site-header.tsx
+++ components/Dashboard/site-header.tsx
@@ -1,3 +1,5 @@
+"use client";
+import React, { useState } from "react";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { RepoDropdown } from "./repo-dropdown";
@@ -5,6 +7,7 @@ import { RepoDropdown } from "./repo-dropdown";
 const versions = ["repo-1", "repo-2", "repo-3"];
 
 export function SiteHeader() {
+  const [showSaved, setShowSaved] = useState(false);
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -16,7 +19,9 @@ export function SiteHeader() {
         <h1 className="text-base font-medium">
           <RepoDropdown versions={versions} defaultVersion={versions[0]} />
         </h1>
-        <div className="ml-auto flex items-center gap-2">Saving...</div>
+        <div className="ml-auto flex items-center gap-2">
+          {showSaved && "Saved"}
+        </div>
       </div>
     </header>
   );
