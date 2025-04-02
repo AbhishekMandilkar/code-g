55e3f5453f8b6d4de8c65a046b7a76e6f3628926
Shlok0606 <shlokpoona@gmail.com>
2025-04-02 11:43:13 +0530

    feat: added basic dashboard

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,8 +1,9 @@
 "use-client";
+import Page from "@/app/dashboard/page";
 import React from "react";
 
 function Main() {
-  return <div>Main</div>;
+  return <div>{Page()}</div>;
 }
 
 export default Main;
